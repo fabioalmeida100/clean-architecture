87ea179bd925cddc2f072b456b9cd1e606594884
Fábio Almeida <fabioalmeida@fabiodeveloper.com>
2025-08-03 22:32:14 -0300

    feat: add list product usecase

---
+++ src/usecase/product/list/list.product.unit.spec.ts
@@ -0,0 +1,56 @@
+import ProductFactory from "../../../domain/product/factory/product.factory";
+import ListProductUseCase from "./list.product.usecase";
+
+const product1 = ProductFactory.create("a", "Product A", 100);
+const product2 = ProductFactory.create("b", "Product B", 200);
+
+const MockRepository = () => {
+    return {
+        find: jest.fn(),
+        findAll: jest.fn().mockReturnValue(Promise.resolve([product1, product2])),
+        create: jest.fn(),
+        update: jest.fn(),
+    };
+}
+
+describe("Unit Test list product use case", () => {
+    it("should list a product", async () => {
+        // Arrange
+        const productRepository = MockRepository();
+        const listProductUseCase = new ListProductUseCase(productRepository);
+
+        // Act
+        const output = await listProductUseCase.execute({});
+
+        // Assert
+        expect(output).toEqual({
+            products: [
+                {
+                    id: product1.id,
+                    name: product1.name,
+                    price: product1.price,
+                },
+                {
+                    id: product2.id,
+                    name: product2.name,
+                    price: product2.price,
+                },
+            ],
+        });
+    });
+    
+    it("should not list a product", async () => {
+        // Arrange
+        const productRepository = MockRepository();
+        productRepository.findAll.mockImplementation(() => {
+            throw new Error("Product not found");
+        });
+        const listProductUseCase = new ListProductUseCase(productRepository);
+
+        // Act
+        const input = {};
+
+        // Assert
+        await expect(listProductUseCase.execute(input)).rejects.toThrow("Product not found");
+    });
+});
